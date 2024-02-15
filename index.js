00660abcc1e9b96102c4effe5e7412c020fe0d7f
zahid072 <jahidhasan19368@gmail.com>
2024-02-15 21:22:43 +0600

    all complete

--- index.js
+++ index.js
@@ -17,3 +17,40 @@ for (let product of allCards) {
     titleCounter++;
   });
 }
+
+const applyBtn = document.getElementById("apply-btn");
+
+applyBtn.addEventListener("click", function () {
+  const totalPrice = document.getElementById("totalPrice");
+  const totalPriceNumber = parseInt(totalPrice.innerText);
+  const discountPrice = document.getElementById("discountPrice");
+  const total = document.getElementById("total");
+  const inputValue = document.getElementById("input-field").value;
+
+  if (totalPriceNumber >= 200 && inputValue.toLowerCase() === "sell 200") {
+    const discount = (totalPriceNumber * 20) / 100;
+    const totalDiscountPrice = totalPriceNumber - discount;
+    discountPrice.innerText = discount;
+    total.innerText = totalDiscountPrice;
+  } else if (inputValue.toLowerCase() !== "sell 200") {
+    alert("invalid code");
+  } else {
+    alert("For discount minimum purchase $200");
+  }
+  document.getElementById("input-field").value = "";
+});
+
+const modal = document.getElementById("modal");
+document.getElementById("purchaseBtn").addEventListener("click", function () {
+  const totalPrice = document.getElementById("totalPrice");
+  const totalPriceNumber = parseInt(totalPrice.innerText);
+  if (totalPriceNumber > 0) {
+    modal.style.display = "block";
+  } else {
+    alert("Please select product for purchase");
+  }
+});
+
+document.getElementById("goHomeBtn").addEventListener("click", function () {
+  modal.style.display = "none";
+});
